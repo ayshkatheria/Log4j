6a0e988402b7dfda2cf6f1bb7748a3c2473a8cde
katheria Divya <dkatheria@192.168.1.208>
2023-07-16 17:00:27 +0100

    Added log files and java calsses

--- katheria.ayshwarya.loginfopck/src/HelloLog4j.java
+++ katheria.ayshwarya.loginfopck/src/HelloLog4j.java
@@ -1,10 +1,17 @@
 import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+import java.util.Date;
 
 
 public class HelloLog4j {
     static Logger log = Logger.getLogger(HelloLog4j.class);
 
     public static void main(String [] args){
+        Date d = new Date();
+        System.setProperty("current.date",d.toString().replace(":","_").replace(" ","_"));
+
+        PropertyConfigurator.configure("./log4j.properties");
 
         System.out.println("Hello world!!!!!");
         log.debug("This is debug log");
